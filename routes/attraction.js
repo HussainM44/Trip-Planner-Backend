2631a415d88ef08a24c0efb916dfe83f7fe7aae4
HussainM44 <hussainmumtaz2001@gmail.com>
2025-12-23 13:12:16 +0300

    merge conflucts

--- routes/attraction.js
+++ routes/attraction.js
@@ -2,15 +2,12 @@ const router = require("express").Router()
 
 const attractionCont = require("../controllers/attractionCont")
 
-const attractionJSON= require("../attractionJSON")
+const attractionJSON = require("../attractionJSON")
 // Routes
 
 router.get("/countries", attractionCont.getAttractions)
 
 // router.post("/countries",attraction)
-
-
-
 
 // Exports
 module.exports = router
